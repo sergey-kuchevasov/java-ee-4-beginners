9e4768502ba2665e2ed00bda899686a48bda8bf0
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-09 17:32:15 +0300

    Static demo.

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -5,20 +5,34 @@
  */
 package com.acme.edu;
 
+import static com.acme.edu.Calculator.add;
+import static com.acme.edu.Calculator.getResult;
 /**
  *
  * @author eugene
  */
 public class Main {
     public static void main(String[] args) {
-	while(true) {
-		new DataStruct(); 
-	}
+        Calculator.add(1, 1);
+        
+        Calculator calculator = new Calculator();
+        calculator = null;
+        calculator.getResult();
+        
+        System.out.println(getResult());
     }
 }
 
-class DataStruct {
-	private int property = 0;
-	private DataStruct next = null;
-	long[] loadArray = new long[10_000_000];
+class Calculator {
+    private static int result;
+    
+    public static void add(int a, int b) {
+        result = a + b;
+    }
+    
+    public static int getResult() {
+        return result;
+    }
 }
+
+
