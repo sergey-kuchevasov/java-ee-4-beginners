2853a7c506b72a6ea2b6b91d6e9c54f3e70fc31c
SD.Kuchevasov <SD.Kuchevasov@NB-SDKuchevasov.cpr.jet.msk.su>
2017-01-11 09:56:48 +0300

    Change sout message

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -1,4 +1,4 @@
-/*
+/*-DproxySet=true -DproxyHost=193.203.100.139 -DproxyPort=8080
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,6 +11,6 @@ package com.acme.edu;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("test");
     }
 }
