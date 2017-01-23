5bfca97fb67af41b3a204250d29d1f9930dd7d1e
SD.Kuchevasov <SD.Kuchevasov@NB-SDKuchevasov.cpr.jet.msk.su>
2017-01-23 09:16:29 +0300

    Change sout message

--- src/main/java/com/acme/edu/Main.java
+++ src/main/java/com/acme/edu/Main.java
@@ -5,12 +5,38 @@
  */
 package com.acme.edu;
 
+import java.util.Optional;
+
 /**
  *
  * @author eugene
  */
 public class Main {
     public static void main(String[] args) {
+        Optional<Object> opt;
         System.out.println("test");
+        for (;;) {
+            System.out.println("i = ");
+            for (int j = 0; ifbusiness(); j++) {
+                System.out.println("j = " + j);
+                if (j==2) {
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static boolean ifbusiness() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
